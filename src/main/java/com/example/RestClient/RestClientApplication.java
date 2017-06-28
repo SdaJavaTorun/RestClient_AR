3aac89384ffb3dee71fc037ef0b2aa7fb6115ed2
grovas <arczir@gmail.com>
2017-06-28 18:04:13 +0200

    init commit

---
+++ src/main/java/com/example/RestClient/RestClientApplication.java
@@ -0,0 +1,14 @@
+package com.example.RestClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestClientApplication implements Consol{
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestClientApplication.class, args);
+	}
+
+
+}
